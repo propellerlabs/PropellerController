a0686042745ec5e4884a0c23af856bd714ca090f
nadohs <djsente@gmail.com>
2017-01-26 09:47:04 -0800

    Initial Commit

---
+++ PropellerController/PropellerController.h
@@ -0,0 +1,19 @@
+//
+//  PropellerController.h
+//  PropellerController
+//
+//  Created by RGfox on 1/26/17.
+//  Copyright © 2017 Propeller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PropellerController.
+FOUNDATION_EXPORT double PropellerControllerVersionNumber;
+
+//! Project version string for PropellerController.
+FOUNDATION_EXPORT const unsigned char PropellerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PropellerController/PublicHeader.h>
+
+
